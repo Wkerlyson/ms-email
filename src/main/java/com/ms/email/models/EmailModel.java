5f5d85a9a68425155631aa7255f51ab5785f8dd2
Wkerlyson <wkerlyson.batista@gmail.com>
2023-01-25 13:09:28 -0300

    first commit

---
+++ src/main/java/com/ms/email/models/EmailModel.java
@@ -0,0 +1,28 @@
+package com.ms.email.models;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.io.Serializable;
+import java.time.LocalDateTime;
+
+@Data
+@Entity
+@Table(name = "TB_EMAIL")
+public class EmailModel implements Serializable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long emailId;
+    private String ownerRef;
+    private String emailFrom;
+    private String emailTo;
+    private String subject;
+    @Column(columnDefinition = "TEXT")
+    private String text;
+    private LocalDateTime sendDateEmail;
+    private StatusEmail statusEmail;
+
+    private static final long serialVersionUID = -3013122315662714947L;
+
+
+}
